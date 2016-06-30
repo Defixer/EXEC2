d1ca241d7fffb384fb6fe1ed4ca61584297a24a1
Defixer <jp.mendoza017@gmail.com>
2016-06-30 14:59:26 +0800

    Initial Commit

---
+++ tests/HelloJava2.java
@@ -0,0 +1,10 @@
+public class HelloJava2{
+	public static void main(String args[]){
+		String firstName = args[0];
+		String lastName	= args[1];
+		System.out.println("\nHello "+firstName+" "+lastName);
+		System.out.println("Welcome to Java Programming!");
+		
+		//©
+	}
+}
